27336fadbfecfcd3ef046f3ccb95d7ecfb477f73
Diego López León <dieguitoll@gmail.com>
2020-05-12 16:57:35 -0300

    Use the same message for requesting OAuth token

--- src/main/java/org/iadb/tech/TokenHandlerVerticle.java
+++ src/main/java/org/iadb/tech/TokenHandlerVerticle.java
@@ -13,20 +13,18 @@ import org.w3c.dom.Document;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.ByteArrayInputStream;
-import java.util.HashMap;
-import java.util.Map;
 
 public class TokenHandlerVerticle extends AbstractVerticle {
 
     private static final Logger logger = LoggerFactory.getLogger(TokenHandlerVerticle.class);
-    private static final String OAUTH_REQUEST_FORMAT = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+    private static final String OAUTH_REQUEST = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
             "<oAuthToken\n" +
             "    xmlns=\"http://com.citi.citiconnect/services/types/oauthtoken/v1\">\n" +
             "    <grantType>client_credentials</grantType>\n" +
-            "    <scope>%s</scope>\n" +
+            "    <scope>/authenticationservices/v1</scope>\n" +
             "    <sourceApplication>CCF</sourceApplication>\n" +
             "</oAuthToken>";
-    private final Map<String, Token> tokens = new HashMap<>();
+    private Token currentToken;
 
     @Override
     public void start() throws Exception {
@@ -36,32 +34,27 @@ public class TokenHandlerVerticle extends AbstractVerticle {
 
         EventBus eventBus = vertx.eventBus();
         eventBus.consumer("get_token", (Handler<Message<JsonObject>>) event -> {
-            logger.debug("get_token -> {}", event.body().encodePrettily());
-            String scope = event.body().getString("scope");
-            if (tokens.containsKey(scope) && !tokens.get(scope).isExpired()) {
-                event.reply(tokens.get(scope).getValue());
+            logger.debug("get_token -> (currentToken: {})", currentToken);
+            if (currentToken != null && !currentToken.isExpired()) {
+                event.reply(currentToken.getValue());
             } else {
-                if (tokens.containsKey(scope)) {
+                if (currentToken != null) {
                     logger.info("OAuth token expired, requesting a new one");
                 }
                 eventBus.request(
                     "citi_connect",
                     new JsonObject()
                             .put("uri", "/citiconnect/sb/authenticationservices/v2/oauth/token")
-                            .put("request", String.format(OAUTH_REQUEST_FORMAT, scope)),
+                            .put("request", OAUTH_REQUEST),
                     (Handler<AsyncResult<Message<String>>>) reply -> {
                         if (reply.succeeded()) {
                             try {
                                 Document tokenResponse = tokenResponseBuilder.parse(new ByteArrayInputStream(reply.result().body().getBytes()));
                                 String tokenValue = tokenResponse.getElementsByTagName("access_token").item(0).getTextContent();
                                 Long lifetime = Long.valueOf(tokenResponse.getElementsByTagName("expires_in").item(0).getTextContent());
-                                String tokenScope = tokenResponse.getElementsByTagName("scope").item(0).getTextContent();
-                                if (!scope.equals(tokenScope)) {
-                                    logger.warn("Scope requested ({}) doesn't match scope authorized ({})", scope, tokenScope);
-                                }
-                                Token newToken = new Token(tokenValue, lifetime);
-                                logger.info("New OAuth token: {}", newToken);
-                                tokens.put(scope, newToken);
+
+                                currentToken = new Token(tokenValue, lifetime);
+                                logger.info("New OAuth token: {}", currentToken);
                                 event.reply(tokenValue);
                             } catch (Exception e) {
                                 logger.error("Unable to parse token response", e);
