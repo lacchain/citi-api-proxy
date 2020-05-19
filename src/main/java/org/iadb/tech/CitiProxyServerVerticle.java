393924f1331122a8e232cc44d442387ebd45f69a
Diego López León <dieguitoll@gmail.com>
2020-05-19 11:39:15 -0300

    Move HTTP Server intialization to its own verticle

---
+++ src/main/java/org/iadb/tech/CitiProxyServerVerticle.java
@@ -0,0 +1,63 @@
+package org.iadb.tech;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CitiProxyServerVerticle extends AbstractVerticle {
+
+    private static final Logger logger = LoggerFactory.getLogger(CitiProxyServerVerticle.class);
+
+    @Override
+    public void start() {
+        HttpServer server = vertx.createHttpServer();
+        Router router = Router.router(vertx);
+        router.route().handler(BodyHandler.create());
+        router.route().handler(routingContext -> {
+            String citiConnectPath = routingContext.request().path();
+            HttpServerResponse response = routingContext.response();
+            vertx.eventBus().request(
+                    "get_token", null,
+                    (Handler<AsyncResult<Message<String>>>) getToken -> {
+                        if (getToken.succeeded()) {
+                            vertx.eventBus().request(
+                                    "citi_connect",
+                                    new JsonObject()
+                                            .put("uri", citiConnectPath)
+                                            .put("token", getToken.result().body())
+                                            .put("request", routingContext.getBodyAsString()),
+                                    (Handler<AsyncResult<Message<String>>>) citiConnect -> {
+                                        if (citiConnect.succeeded()) {
+                                            response
+                                                    .putHeader(HttpHeaders.CONTENT_TYPE, "application/xml")
+                                                    .end(citiConnect.result().body());
+                                        } else {
+                                            response.setStatusCode(500).setStatusMessage("Internal Server Error").end();
+                                        }
+                                    }
+                            );
+                        } else {
+                            response.setStatusCode(500).setStatusMessage("Internal Server Error").end();
+                        }
+                    }
+            );
+        });
+
+        server.requestHandler(router).listen(8080, listen -> {
+            if (listen.succeeded()) {
+                logger.info("Server is now listening!");
+            } else {
+                logger.error("Failed to bind!", listen.cause());
+            }
+        });
+    }
+}
